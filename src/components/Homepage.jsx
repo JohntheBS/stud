120295260ddbbf243317d24b40fe8c3d3526fba8
Hedde Ebong <heddeebong@gmail.com>
2022-08-12 12:07:13 +0100

    fixes

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,16 +4,16 @@ const Homepage = () => {
     return (
         <div className=" font-sans bg-slate-200 text-white">
             <div className='homepahe w-full lg:h-[70vh] h-fit bg-[url("./assets/images/home.jpg")] bg-center bg-cover ' >
-                <div className= " hero bg-black/50 backdrop-blur-[1 px] w-[100%] h-full flex-col py-[12%] text-center p-4 gap-10 ">
-                    <h1 className="lg:text-7xl  text-4xl capitalize sm:text-5xl">best free books and online resourses on the web for telecom students</h1>
-                    <p className="text-md font-light Lg:text-xl lg:w-[50%] mx-auto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos obcaecati nam consectetur inventore fugit sed recusandae tenetur natus officia dicta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum facere commodi illum impedit, officia distinctio vero porro laboriosam at ex.</p>
+                <div className= " hero bg-black/50 backdrop-blur-[.5pxpx] w-[100%] h-full flex-col py-[5%] text-center p-4 gap-10 ">
+                    <h1 className="lg:text-8xl  text-4xl  capitalize sm:text-5xl">best free books and online resourses on the web for telecom students</h1>
+                    <p className="text-md font-light lg:text-3xl lg:w-[50%] mx-auto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos obcaecati nam consectetur inventore fugit sed recusandae tenetur natus officia dicta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum facere commodi illum impedit, officia distinctio vero porro laboriosam at ex.</p>
                     <div className="flex justify-center my-2">
                         <div className="bg-[url('./assets/images/1.jpg')] w-10 rounded-full h-10 lg:h-16 lg:w-16 bg-center bg-cover" ></div>
                         <div className="bg-[url('./assets/images/2.jpg')] w-10 rounded-full h-10 lg:h-16 lg:w-16 bg-center bg-cover ml-[-5px]" ></div>
                         <div className="bg-[url('./assets/images/3.jpg')] w-10 rounded-full h-10 lg:h-16 lg:w-16 bg-center bg-cover ml-[-5px]" ></div>
-                        <p>are all benefitimg from our <span className="text-blue-500">1000 books and resourses</span></p>
+                        <p className="text-center text-lg py-4">are all benefitimg from our <span className="text-blue-500">1000 books and resourses</span></p>
                     </div>
-                    <button><Link className="bg-blue-500 p-2 text-md md:text-lg lg:p-4 lg:text-xl font-bold block border-2 rounded-md uppercase border-blue-500 hover:bg-gray-900" to={"/"}>start learning</Link></button>
+                    <button><Link className="bg-blue-500 p-2 text-md md:text-lg lg:p-8 lg:text-xl font-bold block border-2 rounded-md uppercase border-blue-500 hover:bg-gray-900" to={"/"}>start learning</Link></button>
 
                 </div>
             </div>
