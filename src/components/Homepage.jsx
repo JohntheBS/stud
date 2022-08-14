32691102256d4e80150c00fe9b46500cd244c098
Hedde Ebong <heddeebong@gmail.com>
2022-08-14 01:45:26 +0100

    skjdhsudhuhd

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Homepage = () => {
     return (
         <div className=" font-sans bg-slate-200 text-white">
-            <div className='homepahe w-full lg:h-[70vh] h-fit bg-[url("./assets/images/home.jpg")] bg-center bg-cover ' >
+            <div className='homepahe w-full lg:h-fit          h-fit bg-[url("./assets/images/home.jpg")] bg-center bg-cover ' >
                 <div className= " hero bg-black/50 backdrop-blur-[.5pxpx] w-[100%] h-full flex-col py-[5%] text-center p-4 gap-10 ">
                     <h1 className="lg:text-8xl  text-4xl  capitalize sm:text-5xl">best free books and online resourses on the web for telecom students</h1>
                     <p className="text-md font-light lg:text-3xl lg:w-[50%] mx-auto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos obcaecati nam consectetur inventore fugit sed recusandae tenetur natus officia dicta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum facere commodi illum impedit, officia distinctio vero porro laboriosam at ex.</p>
@@ -19,28 +19,27 @@ const Homepage = () => {
             </div>
             <div className="next my-4">
                 <h1 className="text-2xl text-center mb-4 text-black">Demo Courses</h1>
-                <div className="courses flex gap-4 flex-wrap justify-center">
-                    <div className="h-[410px] w-[400px] shadow-md shadow-black text-gray-900 bg-white ">
+                <div className="courses fgrid grid-cols-4 lg:grid-cols-4 gap-4 sm:grid sm:grid-cols-2  flex-wrap justify-center">
+                    <div className="h-[410px]  shadow-md shadow-black text-gray-900 bg-white ">
                         <div className="bg-white  h-[60%] bg-center bg-cover bg-[url('./assets/images/pic.jpg')]"></div>
                         <h1 className=" text-lg  capitalize text-center">ip phone configs</h1>
                         <h1>name:The boy</h1>
                         <h1 className="font-thin">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum beatae officia commodi repellendus voluptate totam inventore in placeat facilis dolores, minus reprehenderit incidunt dolor atque ad. Aliquid cumque ut nihil.</h1>
                     </div>
-                <div className="courses flex gap-4 flex-wrap justify-center">
-                    <div className="h-[410px] w-[400px] shadow-md shadow-black text-gray-900 bg-white ">
+                    <div className="h-[410px]  shadow-md shadow-black text-gray-900 bg-white ">
                         <div className="bg-white  h-[60%] bg-center bg-cover bg-[url('./assets/images/pic.jpg')]"></div>
                         <h1 className=" text-lg  capitalize text-center">ip phone configs</h1>
                         <h1>name:The boy</h1>
                         <h1 className="font-thin">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum beatae officia commodi repellendus voluptate totam inventore in placeat facilis dolores, minus reprehenderit incidunt dolor atque ad. Aliquid cumque ut nihil.</h1>
                     </div>
-                    </div>
-                    <div className="h-[410px] w-[400px] shadow-md shadow-black text-gray-900 bg-white ">
+                
+                    <div className="h-[410px]  shadow-md shadow-black text-gray-900 bg-white ">
                         <div className="bg-white  h-[60%] bg-center bg-cover bg-[url('./assets/images/pic2.jpg')]"></div>
                         <h1 className=" text-lg  capitalize text-center">Private network using MSAN</h1>
                         <h1>name:John</h1>
                         <h1 className="font-thin">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum beatae officia commodi repellendus voluptate totam inventore in placeat facilis dolores, minus reprehenderit incidunt dolor atque ad. Aliquid cumque ut nihil.</h1>
                     </div>
-                    <div className="h-[410px] w-[400px] shadow-md shadow-black text-gray-900 bg-white ">
+                    <div className="h-[410px]  shadow-md shadow-black text-gray-900 bg-white ">
                         <div className="bg-white  h-[60%] bg-center bg-cover bg-[url('./assets/images/pic4.jpg')]"></div>
                         <h1 className=" text-lg  capitalize text-center">ip phone configs</h1>
                         <h1>name:Emmanuel</h1>
